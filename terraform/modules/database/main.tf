42d277a7642904c10aa69ec8c925520b6c567ae7
Ahmed Mostafa <aboalnaderahmed@gmail.com>
2025-11-06 16:06:30 +0200

    2

--- terraform/modules/database/main.tf
+++ terraform/modules/database/main.tf
@@ -3,11 +3,11 @@ resource "google_compute_global_address" "private_ip_range" {
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
   prefix_length = 16
-  network       = "projects/${var.project_id}/global/networks/${var.vpc_name}"
+  network       = var.vpc_self_link
 }
 
 resource "google_service_networking_connection" "private_vpc_connection" {
-  network                 = "projects/${var.project_id}/global/networks/${var.vpc_name}"
+  network                 = var.vpc_self_link
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.private_ip_range.name]
 
