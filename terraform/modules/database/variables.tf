ca863ede3bf1db82ec6b7d37758cc301e71c3839
Ahmed Mostafa <aboalnaderahmed@gmail.com>
2025-11-06 15:57:48 +0200

    1

--- terraform/modules/database/variables.tf
+++ terraform/modules/database/variables.tf
@@ -1,7 +1,7 @@
 variable "db_instance_name" { type = string }
 variable "region" { type = string }
 variable "project_id" { type = string }
-variable "vpc_name" { type = string }
+# variable "vpc_name" { type = string }
 variable "db_name" { type = string }
 variable "db_user" { type = string }
 variable "db_password" { type = string }
