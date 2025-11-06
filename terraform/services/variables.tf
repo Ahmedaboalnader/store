99bb5c933f13b1e3689f33a151b173dcdc62d865
Ahmed Mostafa <aboalnaderahmed@gmail.com>
2025-11-06 16:23:04 +0200

    3

--- terraform/services/variables.tf
+++ terraform/services/variables.tf
@@ -7,3 +7,23 @@ variable "region" {
   type    = string
   default = "us-central1"
 }
+
+variable "backend_image" {
+  description = "Docker image for the backend"
+  type        = string
+}
+
+variable "frontend_image" {
+  description = "Docker image for the frontend"
+  type        = string
+}
+
+variable "db_private_ip" {
+  description = "Private IP of the database"
+  type        = string
+}
+
+variable "vpc_connector_id" {
+  description = "VPC connector ID for Cloud Run"
+  type        = string
+}
