8e2e6ccef8936a303f3d2ce90b409c73ea2e335b
Ahmed Mostafa <aboalnaderahmed@gmail.com>
2025-11-06 17:31:24 +0200

    add output

--- terraform/services/outputs.tf
+++ terraform/services/outputs.tf
@@ -1,7 +1,12 @@
+
+output "frontend_url" {
+  value = module.frontend.frontend_url
+}
+
 output "backend_url" {
   value = module.backend.backend_url
 }
 
-output "frontend_url" {
-  value = module.frontend.frontend_url
+output "backend_service_name" {
+  value = module.backend.backend_service_name
 }
