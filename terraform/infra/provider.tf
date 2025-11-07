30ecad7806c0fc3e3bf7af1ce30f25030233bbb3
Ahmed Mostafa <aboalnaderahmed@gmail.com>
2025-11-08 01:51:02 +0200

    11

--- terraform/infra/provider.tf
+++ terraform/infra/provider.tf
@@ -11,13 +11,7 @@ terraform {
   # Compatible with Terraform 1.x (workflow uses 1.13.0)
   required_version = ">= 1.0.0, < 2.0.0"
 
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "ahmedaboalnder"
-    workspaces {
-      name = "my-infra"
-    }
-  }
+
 }
 
 provider "google" {
