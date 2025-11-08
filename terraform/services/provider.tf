b83617a9af2b6f08c6a9ee53345e62283d217e94
Ahmed Mostafa <aboalnaderahmed@gmail.com>
2025-11-08 02:09:42 +0200

    13

--- terraform/services/provider.tf
+++ terraform/services/provider.tf
@@ -9,13 +9,7 @@ terraform {
   # Require Terraform 1.x (workflow uses 1.13.0)
   required_version = ">= 1.0.0, < 2.0.0"
 
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "ahmedaboalnder"
-    workspaces {
-      name = "my-services"
-    }
-  }
+
 }
 
 provider "google" {
